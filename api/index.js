61ed0b3757a11e2bc4c5360f4f3cd27fab82a271
Iamniss1234 <thilakshananiss1234@gmail.com>
2024-02-26 18:42:51 +0530

    added

--- api/index.js
+++ api/index.js
@@ -1,5 +1,14 @@
-import express from 'express';
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log("connected to mongodb database!");
+}).catch((err)=>{
+    console.log("error connecting to mongodb database");
+});
 const app = express();
 
 app.listen(3000, ()=>{
